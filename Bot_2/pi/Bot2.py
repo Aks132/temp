d0cf1f19a5eb60a03ccee8fbc90c26745b4e8c72
Anil <anilmaity18@gmail.com>
2021-08-02 12:28:05 +0530

    unified script

--- Bot_2/pi/Bot2.py
+++ Bot_2/pi/Bot2.py
@@ -6,6 +6,9 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+reqdist1 = 60
+reqdist2 = 50
+
 PWM = [20, 17, 23, 5]
 DIG = [21, 27, 24, 6]
 Soft_PWM = []
@@ -15,18 +18,31 @@ side = 0
 
 # ----------------------------------------------------
 Yaw = 0
-dist1 = 0
+frontdist = 0
+sidedist = 0
+
 P_S = 0.0  # 0.3
 P_Y = 0
+P_F = 0
+
 D_F = 0
+D_Side = 0
+D_Front = 0
 D_Y = 0
+
 I_Yaw = 0
 I_Y = 0
+I_Front = 0
+I_F = 0
+I_Side = 0
+I_S = 0
+
 offsetyaw = 0
 presetyaw = 0
+
 D_error = 0
-error_dist1 = 0
-reqdist1 = 60
+DF_error = 0
+DS_error = 0
 
 try:
     ser1 = serial.Serial('/dev/ttyACM1', 115200, timeout=0.1)  # change name, if needed
@@ -39,17 +55,17 @@ except:
 
 
 def Com_Arduino():
-    global Yaw, dist1, dist2
+    global Yaw, frontdist, sidedist
     try:
 
         response = ser1.readline().decode('utf-8').rstrip()
         print(response)
-        l = str(response).split('@')  ##@Yaw@dist1@dist2@##
+        l = str(response).split('@')  ##@Yaw@frontdist@sidedist@##
         if len(l) <= 4 and len(response) > 12:
             Yaw = float(l[1]) + offsetyaw + presetyaw
-            dist1 = float(l[2])
-            dist2 = float(l[2])
-            print('Yaw ' + str(Yaw) + ' dist1 ' + str(dist1)) + ' dist1 ' + str(dist2)
+            frontdist = float(l[2])
+            sidedist = float(l[3])
+            print('Yaw ' + str(Yaw) + ' frontdist ' + str(frontdist)) + ' frontdist ' + str(sidedist)
     except:
         print("Yaw error")
 
@@ -69,7 +85,7 @@ def motor_setup():
 
 
 def motor_feed(speed, rotate, side):
-    global errordist1, I_Y, I_Yaw, D_Yaw, D_error, error_dist1, D_Y, presetyaw
+    global I_Y, I_Yaw, D_Yaw, D_error, D_Y, presetyaw, P_F, Yaw, reqdist2, I_Front, I_F, I_S, I_Side, DF_error, DS_error, D_F, D_Front, D_Side
     jsVal = js.getJS()
 
     if jsVal['o'] == 1:
@@ -85,31 +101,41 @@ def motor_feed(speed, rotate, side):
         D_Y = 1
         presetyaw = -Yaw
         Yaw = 0
-        time.sleep(1)
+        time.sleep(0.5)
 
-    '''
         if jsVal['s'] == 1:
             P_S = 0.1
-        elif jsVal['t'] == 1 or P1 == 1 or P2 == 1:
+        elif jsVal['t'] == 1:
             P_S = 0
-            stop()
-    '''
 
-    error_dist1 = (reqdist1 - dist1)
-    erroryaw = Yaw
+    # YAW
     I_Yaw += I_Yaw * I_Y
     D_Yaw = (D_error - Yaw)
     D_error = Yaw
     outyaw = P_Y * (Yaw) + I_Yaw + D_Yaw
 
-    outSide = error_dist1 * P_S
+    # Side
+    error_dist1 = (reqdist1 - frontdist).P_S
+    I_Side += I_Side * I_S
+    D_Side = (D_error - frontdist)
+    DS_error = sidedist
+    outfront = 0  # error_dist1+I_Front+D_Front
 
+    # Front
+    error_dist2 = (reqdist2 - sidedist)
+    I_Front += I_Front * I_F
+    D_Front = (D_error - sidedist)
+    DF_error = frontdist
+    outside = 0  # error_dist2+I_Side+D_Side
+
+    outSide = error_dist1 * P_S
+    outFront = error_dist2 * P_F
     # print(str(int(outyaw)) + " " + str(int(outSide)) + " " + str(int(rotate)))
 
-    speedm1 = int(speed + rotate + side + outyaw + outSide + (speed * 0.0))
-    speedm2 = int(speed - rotate - side - outyaw - outSide + (speed * 0.0))
-    speedm3 = int(speed + rotate - side + outyaw - outSide + (speed * 0.0))
-    speedm4 = int(speed - rotate + side - outyaw + outSide + (speed * 0.0))
+    speedm1 = int(speed + rotate + side + outyaw + outSide + outfront + (speed * 0.0))
+    speedm2 = int(speed - rotate - side - outyaw - outSide + outfront + (speed * 0.0))
+    speedm3 = int(speed + rotate - side + outyaw - outSide + outfront + (speed * 0.0))
+    speedm4 = int(speed - rotate + side - outyaw + outSide + outfront + (speed * 0.0))
     Speed = [speedm1, speedm2, speedm3, speedm4]
     # print(Speed)
     i = 0
@@ -138,8 +164,8 @@ def get_input():
     global speed, offsetyaw, side
     jsVal = js.getJS()
     speed = (jsVal['axis2']) * 70
-    offsetyaw = (jsVal['axis3']) * 20
-    side = (-(jsVal['axis5'] * 70) + (jsVal['axis6'] * 70)) / 2
+    offsetyaw = (jsVal['axis1']) * 20
+    side = (jsVal['axis3']) * 20
 
     # print(str(speed)+" "+str(offsetyaw)+ " "+str(side)+" " + str(P_Y))
 
