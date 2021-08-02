4d5c2937f58b490416e19d89d2e4ef7279bdfdae
Anil <anilmaity18@gmail.com>
2021-08-02 11:52:09 +0530

    unified script

--- Bot_2/pi/Bot2.py
+++ Bot_2/pi/Bot2.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import joy as js
-import Com_Arduino
-import Auto_mode
+import serial
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -13,9 +13,54 @@ speed = 0
 offsetyaw = 0
 side = 0
 
+# ----------------------------------------------------
+Yaw = 0
+dist1 = 0
+P_S = 0.0  # 0.3
+P_Y = 0
+D_F = 0
+D_Y = 0
+I_Yaw = 0
+I_Y = 0
+offsetyaw = 0
+presetyaw = 0
+D_error = 0
+error_dist1 = 0
+reqdist1 = 60
+
+try:
+    ser1 = serial.Serial('/dev/ttyACM1', 115200, timeout=0.1)  # change name, if needed
+
+except:
+    try:
+        ser1 = serial.Serial('/dev/ttyACM0', 115200, timeout=0.1)
+    except:
+        print("/dev/tty Port issue")
+
+
+def Com_Arduino():
+    global Yaw, dist1, dist2
+    try:
+
+        response = ser1.readline().decode('utf-8').rstrip()
+        print(response)
+        l = str(response).split('@')  ##@Yaw@dist1@dist2@##
+        if len(l) <= 4 and len(response) > 12:
+            Yaw = float(l[1]) + offsetyaw + presetyaw
+            dist1 = float(l[2])
+            dist2 = float(l[2])
+            print('Yaw ' + str(Yaw) + ' dist1 ' + str(dist1)) + ' dist1 ' + str(dist2)
+    except:
+        print("Yaw error")
+
+    # message = "P1=" + str(0) + "@" + "P2=" + str(0) + "@" + "A1=0\r"
+    # ser1.write(message.encode('utf-8'))
+
 
 def motor_setup():
     for x in DIG:
+        GPIO.setup(x, GPIO.OUT)  # set pin as output
+    for x in PWM:
         GPIO.setup(x, GPIO.OUT)  # set pin as output
     for y in range(0, 4):
         Soft_PWM.append(GPIO.PWM(PWM[y], 300))  # set pwm frequenvy for M1
@@ -24,12 +69,47 @@ def motor_setup():
 
 
 def motor_feed(speed, rotate, side):
-    # print(str(int(outyaw)) + " " + str(int(speed)) + " " + str(int(rotate)))
+    global errordist1, I_Y, I_Yaw, D_Yaw, D_error, error_dist1, D_Y, presetyaw
+    jsVal = js.getJS()
 
-    speedm1 = int(speed + rotate + side)  # + outyaw + outSide + (speed * 0.0))
-    speedm2 = int(speed - rotate - side)  # - outyaw - outSide + (speed * 0.0))
-    speedm3 = int(speed + rotate - side)  # + outyaw - outSide + (speed * 0.0))
-    speedm4 = int(speed - rotate + side)  # - outyaw + outSide + (speed * 0.0))
+    if jsVal['o'] == 1:
+        P_Y = 0
+        I_Y = 0
+        I_Yaw = 0
+        D_Y = 0
+        D_error = 0
+
+    if jsVal['x'] == 1 and P_Y != 2.5:
+        P_Y = 2.5
+        I_Y = 0.001
+        D_Y = 1
+        presetyaw = -Yaw
+        Yaw = 0
+        time.sleep(1)
+
+    '''
+        if jsVal['s'] == 1:
+            P_S = 0.1
+        elif jsVal['t'] == 1 or P1 == 1 or P2 == 1:
+            P_S = 0
+            stop()
+    '''
+
+    error_dist1 = (reqdist1 - dist1)
+    erroryaw = Yaw
+    I_Yaw += I_Yaw * I_Y
+    D_Yaw = (D_error - Yaw)
+    D_error = Yaw
+    outyaw = P_Y * (Yaw) + I_Yaw + D_Yaw
+
+    outSide = error_dist1 * P_S
+
+    # print(str(int(outyaw)) + " " + str(int(outSide)) + " " + str(int(rotate)))
+
+    speedm1 = int(speed + rotate + side + outyaw + outSide + (speed * 0.0))
+    speedm2 = int(speed - rotate - side - outyaw - outSide + (speed * 0.0))
+    speedm3 = int(speed + rotate - side + outyaw - outSide + (speed * 0.0))
+    speedm4 = int(speed - rotate + side - outyaw + outSide + (speed * 0.0))
     Speed = [speedm1, speedm2, speedm3, speedm4]
     # print(Speed)
     i = 0
@@ -58,7 +138,7 @@ def get_input():
     global speed, offsetyaw, side
     jsVal = js.getJS()
     speed = (jsVal['axis2']) * 70
-    offsetyaw = (jsVal['axis3']) * 40
+    offsetyaw = (jsVal['axis3']) * 20
     side = (-(jsVal['axis5'] * 70) + (jsVal['axis6'] * 70)) / 2
 
     # print(str(speed)+" "+str(offsetyaw)+ " "+str(side)+" " + str(P_Y))
@@ -70,34 +150,3 @@ if __name__ == '__main__':
         get_input()
         Com_Arduino()
         motor_feed(speed, offsetyaw, side)
-
-
-    # try:
-
-    # except:
-    #   stop()
-    #  print("Exception Errrorrrrrrrrrrrrrrrrrrrrrrrrrr ")
-
-'''
-    if jsVal['o'] == 1 or P1 == 1 or P2 == 1:
-        P_Y = 0
-        I_Y = 0
-        I_Yaw = 0
-        D_Y = 0
-        D_error = 0
-
-    if jsVal['s'] == 1:
-        P_S = 0.1
-    elif jsVal['t'] == 1 or P1 == 1 or P2 == 1:
-        P_S = 0
-        stop()
-
-    if jsVal['x'] == 1 and P_Y != 2.5:
-        P_Y = 2.5
-        I_Y = 0.001
-        D_Y = 1
-        presetyaw = -Yaw
-        Yaw = 0
-        time.sleep(1)
-
-'''
