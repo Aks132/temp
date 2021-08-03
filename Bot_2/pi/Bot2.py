9fc485a51beb4b34af65cbc3331088a21f692597
Anilmaity <anilmaity18@gmail.com>
2021-08-03 16:30:59 +0530

    modify

--- Bot_2/pi/Bot2.py
+++ Bot_2/pi/Bot2.py
@@ -6,30 +6,38 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-reqfront = 150
+reqfront = 30
 reqside = 176
 
+Poddistlist = [(145),(310),(380),(545)]
+pos = 1
 PWM = [20, 17, 23, 5]
 DIG = [21, 27, 24, 6]
 Soft_PWM = []
 speed = 0
 offsetyaw = 0
 side = 0
+countpod = 0
 
 # ----------------------------------------------------
 Yaw = 0
 frontdist = 0
 sidedist = 0
+sidedist2 = 0
+ang_err =0
 
 P_S = 0.0  # 0.3
 P_Y = 0
 P_F = 0
+P_ER = 0
 
 D_F = 0
 D_Side = 0
 D_Front = 0
 D_Y = 0
 
+I_A = 0
+I_ang =0
 I_Yaw = 0
 I_Y = 0
 I_Front = 0
@@ -55,7 +63,7 @@ except:
 
 
 def Com_Arduino():
-    global Yaw, frontdist, sidedist
+    global Yaw, frontdist, sidedist,sidedist2,ang_err
     #try:
 
     response = ser1.readline().decode('utf-8').rstrip()
@@ -68,8 +76,10 @@ def Com_Arduino():
         frontdist = float(l[3])
         
         sidedist = float(l[2])
+        sidedist2= float(l[4])
+        ang_err = sidedist-sidedist2 
         #print(sidedist)
-        print('Yaw ' + str(int(Yaw))+" " +str(presetyaw)+ ' frontdist ' + str(frontdist) + ' sidedist ' + str(sidedist))
+        print('Yaw ' + str(int(Yaw))+" " +str(presetyaw)+ ' frontdist ' + str(frontdist) + ' sidedist ' + str(sidedist)+ 'sidedist2 ' + str(sidedist2))
     #except:
        # print("Yaw error")
 
@@ -89,19 +99,25 @@ def motor_setup():
 
 
 def motor_feed(speed, rotate, side):
-    global P_S,P_Y,I_Y, I_Yaw, D_Yaw, D_error, D_Y, presetyaw, P_F, Yaw, reqside
-    global reqfront, I_Front, I_F, I_S, I_Side, DF_error, DS_error, D_F, D_Front, D_Side
+    global P_ER, pos, countpod ,P_S,P_Y,I_Y, I_Yaw, D_Yaw, D_error, D_Y, presetyaw, P_F, Yaw, reqside
+    global I_A , I_ang ,reqfront, I_Front, I_F, I_S, I_Side, DF_error, DS_error, D_F, D_Front, D_Side
     jsVal = js.getJS()
 
-    if jsVal['o'] == 1:
+    if jsVal['o'] == 1 and P_Y == 0.8:
+        presetyaw = 0
+        P_ER = 0
         P_Y = 0
         I_Y = 0
+        I_ang = 0
+        I_A = 0
         I_Yaw = 0
         D_Y = 0
         D_error = 0
 
-    if jsVal['x'] == 1 and P_Y != 1:
+    if jsVal['x'] == 1 and P_Y != 0.8:
+        P_ER = 3
         P_Y = 0.8
+        I_A = 0.001
         I_Y = 0.000
         D_Y = 0.2
         presetyaw = -Yaw
@@ -109,20 +125,35 @@ def motor_feed(speed, rotate, side):
         time.sleep(0.5)
 
     if jsVal['s'] == 1:
-        P_F = 0.1
-        P_S = 0.5
+        P_F = 0.3
+        P_S = 0.3
         I_S = 0.004
     if jsVal['t'] == 1:
         P_F = 0
         P_S = 0
         I_S = 0
         I_Side = 0
+    
+    
+    if(jsVal['R1'] == 1 ):
+        time.sleep(0.1)
+        jsVal = js.getJS()
+        if(jsVal['R1'] == 1 ):
+           
+            reqfront = Poddistlist[countpod]
+            countpod += pos
+            print(countpod)
+            if abs(countpod) >= 3 or countpod <= 0 :
+                pos = -pos
+                
 
     # YAW
     I_Yaw += Yaw * I_Y
     D_Yaw = (D_error - Yaw)
     D_error = Yaw
-    outyaw = P_Y * (Yaw) + I_Yaw+D_Yaw
+    
+    I_ang += ang_err * I_A
+    outyaw = P_ER *ang_err
     #print(str(int(Yaw*P_Y)) + " " + str(int(I_Yaw)) + " " + str(int(D_Yaw)))
 
 
@@ -176,11 +207,13 @@ def stop():
 def get_input():
     global speed, offsetyaw, side
     jsVal = js.getJS()
-    speed = -(jsVal['axis2']) * 70
-    offsetyaw = -(jsVal['axis1']) * 20
-    side = (jsVal['axis3']) * 20
+    speed = -(jsVal['axis2']) * 30
+    offsetyaw = -(jsVal['axis3']) * 20
+    side = (jsVal['axis1']) * 20
+    
+        
 
-    # print(str(speed)+" "+str(offsetyaw)+ " "+str(side)+" " + str(P_Y))
+    print(str(speed)+" "+str(offsetyaw)+ " "+str(side)+" " + str(P_Y))
 
 
 if __name__ == '__main__':
